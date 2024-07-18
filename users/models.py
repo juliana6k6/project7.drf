4d7e1dbd832031e5d73f973b3f7a01f8ce107877
Juliana <juliana6k@mail.ru>
2024-07-18 13:42:35 +0300

    Добавлены файлы

---
+++ users/models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+
+    phone_number = models.CharField(
+        max_length=35,
+        verbose_name="номер телефона",
+        blank=True,
+        null=True,
+        help_text="Укажите номер телефона",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar/",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Загрузите аватар",
+    )
+    tg_nick = models.CharField(
+        max_length=50,
+        verbose_name="Tg_name",
+        blank=True,
+        null=True,
+        help_text="Укажите tg_имя",
+    )
+    country = models.CharField(
+        max_length=100,
+        help_text="Укажите страну",
+        verbose_name="Страна",
+        blank=True,
+        null=True,
+    )
+    city = models.CharField(
+        max_length=100,
+        help_text="Укажите город",
+        verbose_name="Город",
+        blank=True,
+        null=True,
+    )
+    email = models.EmailField(
+        unique=True, verbose_name="Email", help_text="Укажите почту"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
